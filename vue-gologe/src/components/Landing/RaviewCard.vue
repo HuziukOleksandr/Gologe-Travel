0072c82fdf38f264a75496b4ff2d96a4a1959af9
Oleksandr Huziuk <oleksandr.huziuk@gmail.com>
2024-02-27 19:12:46 +0200

    Finished the layout of the Landing page. the next step is to connect the functionality and create a pseudo database

--- vue-gologe/src/components/Landing/RaviewCard.vue
+++ vue-gologe/src/components/Landing/RaviewCard.vue
@@ -6,13 +6,13 @@
 		<h1
 			class="font-serrat font-bold text-2xl text-green"
 		>
-			“A real sense of community, nurtured”
+		“A real sense of community, nurtured”
 		</h1>
 		<div
-			class=""
+			class="flex flex-col"
 		>
 			<p
-				class="h-[42px] font-serrat fonr-regular text-sm text-gray
+				class="h-[22px] font-serrat fonr-regular text-sm text-grey
 				truncate ..."
 			>
 			Really appreciate the help and support from the staff during
@@ -21,16 +21,69 @@
 			when needed.
 			</p>
 			<button
-			class="self-end"
+				class="font-serrat font-bold text-base text-green flex self-end"
 			>
-				View more
+				{{ $t("Landing.Review.more")}}
 			</button>
 		</div>
-		<img src="../../assets/images/Olga.png" alt="">
+
+		<div
+			class="w-[100%] h-[45px] flex gap-[10px]"
+		>
+			<div
+				v-for="index in rates"
+			>
+				<img 
+					src="../../assets/images/star.svg" 
+					alt="star"
+				>
+			</div>
+
+			<div
+				v-for="index in (5-rates)"
+				class=""
+			>
+				<img 
+					src="../../assets/images/star.svg" 
+					alt="star"
+					class="opacity-35"
+				>
+			</div>
+		</div>
+
+		<div
+			class="flex flex-col gap-[5px]"
+		>
+			<h1
+				class="font-serrat font-bold text-[14px] text-green"
+			>
+				Olga
+			</h1>
+			<p
+				class="font-serrat text-[14px] text-grey"
+			>
+				Weave Studios – Kai Tak
+			</p>
+		</div>
+
+		<div
+			class="flex gap-[10px]"
+		>
+			<img src="../../assets/images/google.svg" alt="google">
+			<p
+				class="font-serrat font-bold text-[14px] text-grey"
+			>
+			Google
+			</p>
+		</div>
+
+		<img src="../../assets/images/Olga.png" alt="Olga">
 	</div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+const rates = ref<number>(1)
 
 </script>
 
--- vue-gologe/src/view/Landing.vue
+++ vue-gologe/src/view/Landing.vue
@@ -3,20 +3,20 @@
 		class="max-w-[1440px] w-[100%] m-auto my-[30px]" 
 	>
 		<Header
-			class="max-w-[1380px] w-[100%] x-99 relative"
+			class="max-w-[1380px] w-[100%]  relative z-[99]"
 		/>
 		<Preview 
 			class="mt-[-100px] w-[100%] h-auto z-[0] mx-auto  relative"
 		/>
 		<QuickSearch
-			class="relative mx-auto top-[-100px]  x-99"
+			class="mx-auto top-[-100px] relative z-[99]"
 		/>
 		<Trips 
-			class="relative mx-auto x-99"
+			class=" mx-auto relative z-[99]"
 
 		/>
 		<Review 
-			class="relative mx-auto x-99"
+			class="mx-auto relative z-[99]"
 		/>
 	</div>
 
